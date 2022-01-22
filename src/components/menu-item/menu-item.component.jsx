75d29d25f4529a65c88a60d47d50fcda4fae2c02
Gagan Gautam <gautamgagan525@gmail.com>
2022-01-22 17:04:12 +0530

    update our menu-item to navigate with useNavigate and useLocation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import { useLocation, useNavigate } from "react-router-dom";
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    return (
+    <div className={`${size} menu-item`} onClick={ () => navigate(`${pathname}${linkUrl}`)}>
         <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
         }} />
@@ -12,6 +17,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
-)
+    );
+};
 
 export default MenuItem;
